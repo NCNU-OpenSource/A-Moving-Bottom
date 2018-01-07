56adb5721a1d2d09f938da2e6aa0481dfe6a8ccf
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-07 09:34:05 +0000

    ADC Done

--- testADC.js
+++ testADC.js
@@ -1,12 +1,12 @@
 var mcpadc = require('mcp-spi-adc');
 
-var tempSensor = mcpadc.open(5, {speedHz: 20000}, function (err) {
+var tempSensor = mcpadc.openMcp3008(7, {speedHz: 1350000}, function (err) {
     if (err) throw err;
 
     setInterval(function () {
       tempSensor.read(function (err, reading) {
         if (err) throw err;
-        console.log((reading.value * 3.3 - 0.5) * 100);
+        console.log(reading.value);
       });
     }, 1000);
   });
