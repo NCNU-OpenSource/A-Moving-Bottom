77edfe3227ca1ac97b1b4f2b1e5889a57083f99f
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-07 16:18:49 +0800

    讀取環境設定Pin腳並顯示頁面

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../env');
 var sensor = require('node-dht-sensor');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  sensor.read(22, 4, function(err, temperature, humidity) {
+  sensor.read(22, config.dht_pin, function(err, temperature, humidity) {
     if (!err) {
         console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
             'humidity: ' + humidity.toFixed(1) + '%'
         );
     }
   });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
