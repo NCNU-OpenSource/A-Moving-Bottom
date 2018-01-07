91af517ea93ea3e4cf5c373c727799f5a38bf440
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-07 16:01:49 +0800

    新增DHT22測試

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var sensor = require('node-dht-sensor');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  sensor.read(22, 4, function(err, temperature, humidity) {
+    if (!err) {
+        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%'
+        );
+    }
+  });
 });
 
 module.exports = router;
