f3cabc03788c7822c15efb7dab5977f80c65d843
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-07 17:14:33 +0800

    測試ADC

---
+++ testADC.js
@@ -0,0 +1,12 @@
+var mcpadc = require('mcp-spi-adc');
+
+var tempSensor = mcpadc.open(5, {speedHz: 20000}, function (err) {
+    if (err) throw err;
+
+    setInterval(function () {
+      tempSensor.read(function (err, reading) {
+        if (err) throw err;
+        console.log((reading.value * 3.3 - 0.5) * 100);
+      });
+    }, 1000);
+  });
