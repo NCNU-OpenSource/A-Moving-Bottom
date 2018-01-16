ab6947be3e1d2ebc639a632c58684b638ae98214
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-16 17:41:15 +0800

    Update for testing

--- routes/getINFO.js
+++ routes/getINFO.js
@@ -1,35 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../env');
-var sensor = require('node-dht-sensor');
-var mcpadc = require('mcp-spi-adc');
 
-/* GET INFO of weather. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-    let hot = 0;
-    let wet = 0;
-    let rain = 1;
-    // sensor.read(22, config.dht_pin, function(err, temperature, humidity) {
-    //   if (!err) {
-    //       console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
-    //       hot = temperature.toFixed(1);
-    //       wet = humidity.toFixed(1);
-    //   }
-    // });
-    // var tempSensor = mcpadc.openMcp3008(7, {speedHz: 1350000}, function (err) {
-    //     if (err) throw err;
-    //     tempSensor.read(function (err, reading) {
-    //         if (err) throw err;
-    //         console.log(reading.value);
-    //         rain = reading.value;
-    //     });
-    // });
-    let value = {
-        "temperature": hot,
-        "humidity": wet,
-        "dry_rate": rain
-    };
-    res.send(value);
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
