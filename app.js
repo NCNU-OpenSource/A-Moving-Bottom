d0ec7df827e4a81e243793dd365edb1065c7d0c4
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-16 17:28:48 +0800

    API化

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var config = require('./env');
 
 var index = require('./routes/index');
+var api = require('./routes/getINFO');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
