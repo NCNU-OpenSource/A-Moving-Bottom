a21cde74597b38432598144e198dbfa0e80590a3
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-16 17:54:55 +0800

    Update INFO

--- routes/getINFO.js
+++ routes/getINFO.js
@@ -2,18 +2,29 @@ var express = require('express');
 var router = express.Router();
 var config = require('../env');
 var sensor = require('node-dht-sensor');
+var mcpadc = require('mcp-spi-adc');
 
 /* GET weather api */
 router.get('/', function(req, res, next) {
     var temp = 0;
     var wet = 0;
-    var rainRate = 1;
-    var weatherObj = {
-        "temperature": temp,
-        "humidity": wet,
-        "dry_rate": rainRate
-    };
-//  res.send(weatherObj);
+    var dryRate = 1;
+    sensor.read(22, config.dht_pin, function(err, temperature, humidity) {
+        if (!err) {
+            console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
+            temp = temperature.toFixed(1);
+            wet = humidity.toFixed(1);
+        }
+      });
+    var tempSensor = mcpadc.openMcp3008(7, {speedHz: 1350000}, function (err) {
+        if (err) throw err;
+        tempSensor.read(function (err, reading) {
+            if (err) throw err;
+            console.log(reading.value);
+            dryRate = reading.value;
+        });
+    });
+    res.send({"temperature": temp, "humidity": wet, "dryRate": dryRate});
 });
 
 module.exports = router;
