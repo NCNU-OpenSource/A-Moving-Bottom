d19c5c9f305d3863ba738fcb52c1eca56a4e318c
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-16 17:39:02 +0800

    TestData

--- routes/getINFO.js
+++ routes/getINFO.js
@@ -9,21 +9,21 @@ router.get('/', function(req, res, next) {
     let hot = 0;
     let wet = 0;
     let rain = 1;
-    sensor.read(22, config.dht_pin, function(err, temperature, humidity) {
-      if (!err) {
-          console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
-          hot = temperature.toFixed(1);
-          wet = humidity.toFixed(1);
-      }
-    });
-    var tempSensor = mcpadc.openMcp3008(7, {speedHz: 1350000}, function (err) {
-        if (err) throw err;
-        tempSensor.read(function (err, reading) {
-            if (err) throw err;
-            console.log(reading.value);
-            rain = reading.value;
-        });
-    });
+    // sensor.read(22, config.dht_pin, function(err, temperature, humidity) {
+    //   if (!err) {
+    //       console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
+    //       hot = temperature.toFixed(1);
+    //       wet = humidity.toFixed(1);
+    //   }
+    // });
+    // var tempSensor = mcpadc.openMcp3008(7, {speedHz: 1350000}, function (err) {
+    //     if (err) throw err;
+    //     tempSensor.read(function (err, reading) {
+    //         if (err) throw err;
+    //         console.log(reading.value);
+    //         rain = reading.value;
+    //     });
+    // });
     let value = {
         "temperature": hot,
         "humidity": wet,
