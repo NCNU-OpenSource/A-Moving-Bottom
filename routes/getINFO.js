45e90c609e0a9735580e25b15bad9e2614b86ce3
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-16 17:45:54 +0800

    INFO Testing

--- routes/getINFO.js
+++ routes/getINFO.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../env');
+var sensor = require('node-dht-sensor');
 
-/* GET home page. */
+/* GET weather api */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var temp = 0;
+    var wet = 0;
+    var rainRate = 1;
+    let weatherObj = {
+        "temperature": hot,
+        "humidity": wet,
+        "dry_rate": rain
+    };
+    res.send(weatherObj);
 });
 
 module.exports = router;
