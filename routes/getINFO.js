544dca46fc2df7a2fe3bad87f72cff27606a2e9b
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-16 18:09:41 +0800

    Add promise

--- routes/getINFO.js
+++ routes/getINFO.js
@@ -4,27 +4,52 @@ var config = require('../env');
 var sensor = require('node-dht-sensor');
 var mcpadc = require('mcp-spi-adc');
 
-/* GET weather api */
-router.get('/', function(req, res, next) {
-    var temp = 0;
-    var wet = 0;
-    var dryRate = 1;
-    sensor.read(22, config.dht_pin, function(err, temperature, humidity) {
-        if (!err) {
-            console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
-            temp = temperature.toFixed(1);
-            wet = humidity.toFixed(1);
-        }
-      });
-    var tempSensor = mcpadc.openMcp3008(7, {speedHz: 1350000}, function (err) {
-        if (err) throw err;
-        tempSensor.read(function (err, reading) {
-            if (err) throw err;
-            console.log(reading.value);
-            dryRate = reading.value;
+function getWeather() {
+    return new Promise((resolve, reject) => {
+        sensor.read(22, config.dht_pin, function(err, temperature, humidity) {
+            if (!err) {
+                console.log('temp: ' + temperature.toFixed(1) + '°C, ' + 'humidity: ' + humidity.toFixed(1) + '%');
+                resolve({"temperature": temperature.toFixed(1), "humidity": humidity.toFixed(1)});
+            } else {
+                reject({"error": err});
+            }
         });
     });
-    res.send({"temperature": temp, "humidity": wet, "dryRate": dryRate});
+}
+
+function getDry() {
+    function getWeather() {
+        return new Promise((resolve, reject) => {
+            var tempSensor = mcpadc.openMcp3008(7, {speedHz: 1350000}, function (err) {
+                if(err) {
+                    reject({"error": err});
+                } else {
+                    tempSensor.read(function (err, reading) {
+                        if(err) {
+                            reject({"error": err});
+                        } else {
+                            console.log("dryRate: " + reading.value);
+                            resolve({"dryRate": reading.value});
+                        }
+                    });
+                }
+            });
+        });
+    }
+}
+
+/* GET weather api */
+router.get('/', function(req, res, next) {
+    var temperature, humidity, dryRate;
+    getWeather().then(weatherObject => {
+        temperature = weatherObject.temperature;
+        humidity = weatherObject.humidity;
+    }).then(dryRateObject => {
+        dryRate = dryRateObject.dryRate;
+    }).catch(error => {
+        res.send({"error": error});
+    });
+    res.send({"temperature": temperature, "humidity": humidity, "dryRate": dryRate});
 });
 
 module.exports = router;
