0e1a198489c1d09e856cf3b38ba0d0c06d9c2197
TMineCola <s104213083@mail1.ncnu.edu.tw>
2018-01-16 17:33:57 +0800

    Fixed

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../env');
-var sensor = require('node-dht-sensor');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  sensor.read(22, config.dht_pin, function(err, temperature, humidity) {
-    if (!err) {
-        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-            'humidity: ' + humidity.toFixed(1) + '%'
-        );
-    }
-  });
   res.render('index', { title: 'Express' });
 });
 
